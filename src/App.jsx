74c084eb42b6be706f09e201659d332ee665bd3d
Muhammadullo <mnastulloev@gmail.com>
2023-12-16 07:22:31 +0500

    header_fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
 
 function App() {
-  
-return (
+
+  return (
     <>
 
-     {/*header  */}
-    
+      {/*header  */}
+      <header className='flex justify-between m-[20px]'>
+
+        {/* left */}
+        <div className='mobile:flex mobile:justify-center mobile:items-center'>
+          <img src='src/assets/Logo.svg' />
+        </div>
+
+        {/* right */}
+        <div className='mobile:hidden sm:flex w-[60%] justify-between '>
+          <img className='mr-[20px]' src='src/assets/svg-editor-image-7.svg' />
+          <img className='mr-[20px]' src='src/assets/svg-editor-image-6.svg' />
+          <div className='flex'>
+            <img src='src/assets/Group.svg' />
+            <p>+7 800 301-79-27</p>
+
+          </div>
+          <button className='w-[180px] h-[50px] rounded-[12px] text-[white] bg-[#F05670]'>НАЧАТЬ ПРОЕКТ</button>
+        </div>
+
+      </header>
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': '0px',
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+
+        'lh': '850px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
   },
   plugins: [],
 }
