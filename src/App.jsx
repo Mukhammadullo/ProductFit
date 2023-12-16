cce95b0fdc2f59788721866c06cb4267ed9821f7
Muhammadullo <mnastulloev@gmail.com>
2023-12-16 07:27:22 +0500

    header_fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
     <>
 
       {/*header  */}
-      <header className='flex justify-between m-[20px]'>
+      <header className='flex justify-between m-[30px]'>
 
         {/* left */}
-        <div className='mobile:flex mobile:justify-center mobile:items-center'>
+        <div className='mobile:ml-[20%] sm:ml-0'>
           <img src='src/assets/Logo.svg' />
         </div>
 
         {/* right */}
-        <div className='mobile:hidden sm:flex w-[60%] justify-between '>
-          <img className='mr-[20px]' src='src/assets/svg-editor-image-7.svg' />
-          <img className='mr-[20px]' src='src/assets/svg-editor-image-6.svg' />
+        <div className='mobile:hidden sm:flex w-[50%] justify-between '>
+          <img className=' w-[50px]' src='src/assets/svg-editor-image-7.svg' />
+          <img className=' w-[50px]' src='src/assets/svg-editor-image-6.svg' />
           <div className='flex'>
             <img src='src/assets/Group.svg' />
             <p>+7 800 301-79-27</p>
